1c1d8ae1768d5933159baba5034ab80de546b9b1
FrancescoBeghelli <francesco_jf@hotmail.com>
2018-10-14 10:20:45 -0300

    Projeto criado

---
+++ app/src/main/java/dcc196/ufjf/dcc196_2018_3_exercicio02_francescobeghelli/MainActivity.java
@@ -0,0 +1,13 @@
+package dcc196.ufjf.dcc196_2018_3_exercicio02_francescobeghelli;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
